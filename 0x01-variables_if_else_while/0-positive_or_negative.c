d2ca2a5201c233d0ef26bedab6c81a31982b17f3
gpboguie <stephaneboguie@gmail.com>
2022-09-12 12:20:45 -0700

    positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,12 +1,8 @@
-#include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
-/**
- *  * main - main block
- *   * Description: Get a random number and print the number
- *    * and if it is positive, negative, or zero
- *     * Return: 0
- *      */
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
 int main(void)
 {
 		int n;
